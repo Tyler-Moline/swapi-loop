6f0c5d124289f6a82ed4635bc4bf436eba7e4ea6
Tyler Moline <tmoline@devpipeline.com>
2023-06-22 13:56:37 -0600

    inital commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let info;
+
+async function fetchData() {
+  try {
+    const response = await fetch("https://swapi.dev/api/people");
+    const data = await response.json();
+    info = data["results"];
+    console.log(info);
+  } catch (error) {
+    console.log("error:", error);
+  }
+}
+
+fetchData();
+
+const parentElement = document.body;
+
+function attachToDom() {
+  for (let i = 0; i < 10; i++) {
+    const newP = document.createElement("p");
+    newP.textContent = `${info[i].name}`;
+    parentElement.appendChild(newP);
+  }
+}
+
+start.addEventListener("click", attachToDom);
